2e415c589877e51d3ef6189597214059aab5cb89
Katyayan aanand <katyayan.aanand@gmail.com>
2019-05-13 08:40:19 +0530

    new game functionality

--- app.js
+++ app.js
@@ -5,42 +5,27 @@ GAME RULES:
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 30 points on GLOBAL score wins the game
+- The first player to reach 50 points on GLOBAL score wins the game
 
 */
 var s1 = 0;
 var s2 = 0;
 var curtotal= 0;
-var gamePlaying= true;
+var gamePlaying;
 var activeplayer;
-
-function init(){
-    activeplayer= 0;
-    document.getElementById("current-0").textContent = 0;
-    document.getElementById("current-1").textContent = 0;
-    document.getElementById("score-0").textContent = 0;
-    document.getElementById("score-1").textContent = 0;
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.remove('winner');
-    document.querySelector('.player-0-panel').classList.add('active');
-    document.querySelector('.dice').style.display = 'none';
-}
 
 init();
 
-document.querySelector('.btn-new').addEventListener('click', init);
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying){
         //generate some random number
         var dice = Math.floor(Math.random()*6) + 1; 
-
+        
         //display the result
         document.querySelector('.dice').style.display = 'block';
         document.querySelector('.dice').src = 'dice-' + dice + '.png';
-
+        
         //if !1 then add the result
         if(dice !== 1) {
             curtotal+= dice;
@@ -83,7 +68,24 @@ function nextplayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.getElementById("current-0").textContent = 0;
     document.getElementById("current-1").textContent = 0;
+    
+    document.querySelector('.dice').style.display = 'none';
+}
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    activeplayer= 0;
+    gamePlaying= true;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.dice').style.display = 'none';
 }
 
